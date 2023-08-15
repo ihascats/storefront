f1f70ad17e7cdcc9868013ee9466ffad08215186
ihascats <s.pavlovic.rs@gmail.com>
2023-08-15 12:56:28 +0200

    Incr quantity i/o adding same product 2 cart twice

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -60,13 +60,35 @@ class ProfileController extends Controller
 
         return Redirect::to('/');
     }
-    public function addToCart(Request $request){
-        $user = $request->user();
-        User::find($user->id)->push('cart', [
-            "product_id" => new ObjectId($request->product_id), // Should be objectID
+    public function addToCart(Request $request) {
+
+        $newProduct = [
+            "product_id" => new ObjectId($request->product_id),
             "quantity" => intval($request->quantity)
-        ]);
-        return response()->json(["result" => "ok, added item to cart"], 204);
+        ];
+
+        $user_id = $request->user()->id;
+        $user = User::find($user_id);
+
+        $productInCart = null;
+        $cart = $user->cart ?? [];
+
+        foreach ($cart as $index => $product) {
+            if ($product["product_id"] == $newProduct["product_id"]) {
+                $productInCart = $index;
+                break;
+            }
+        }
+
+        if ($productInCart === null) {
+            $user->push('cart', $newProduct);
+            return response()->json(["result" => "ok, added item to cart"], 204);
+        } else {
+            $cart[$productInCart]["quantity"] += $newProduct["quantity"];
+            $user->cart = $cart;
+            $user->save();
+            return response()->json(["result" => "ok, increased quantity"], 204);
+        }
     }
     public function removeFromCart(Request $request){
         $user = $request->user();
