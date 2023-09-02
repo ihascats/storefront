f4ebcaa8d6703957adce3529f9a222993d01b082
ihascats <s.pavlovic.rs@gmail.com>
2023-09-02 18:13:09 +0200

    Add nav links to the responsive nav menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -98,7 +98,31 @@
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
-
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="auth()->check() ? route('wishlists', ['id' => auth()->user()->id]) : '#'"
+            :active="request()->routeIs('dashboard')">
+                {{ __('Wishlist') }}
+            </x-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="auth()->check() ? route('carts', ['id' => auth()->user()->id]) : '#'" :active="request()->routeIs('dashboard')">
+                {{ __('Cart') }}
+            </x-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('products')" :active="request()->routeIs('dashboard')">
+                {{ __('Products') }}
+            </x-responsive-nav-link>
+        </div>
+        @if(auth()->check())
+            @if(auth()->user()->admin)
+                <div class="pt-2 pb-3 space-y-1">
+                    <x-responsive-nav-link :href="route('products.create')" :active="request()->routeIs('dashboard')">
+                        {{ __('New Product') }}
+                    </x-responsive-nav-link>
+                </div>
+            @endif
+        @endif
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             @if(auth()->check())
