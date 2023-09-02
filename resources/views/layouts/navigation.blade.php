747c656f817fa6ae185bb1eabec4d597cdcbe415
ihascats <s.pavlovic.rs@gmail.com>
2023-09-02 18:04:18 +0200

    Check if there is a user before checking details

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,12 +17,13 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('wishlists', ['id' => auth()->user()->id])" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="auth()->check() ? route('wishlists', ['id' => auth()->user()->id]) : '#'"
+                    :active="request()->routeIs('dashboard')">
                         {{ __('Wishlist') }}
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('carts', ['id' => auth()->user()->id])" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="auth()->check() ? route('carts', ['id' => auth()->user()->id]) : '#'" :active="request()->routeIs('dashboard')">
                         {{ __('Cart') }}
                     </x-nav-link>
                 </div>
@@ -31,12 +32,14 @@
                         {{ __('Products') }}
                     </x-nav-link>
                 </div>
-                @if(auth()->user()->admin)
-                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                        <x-nav-link :href="route('products.create')" :active="request()->routeIs('dashboard')">
-                            {{ __('New Product') }}
-                        </x-nav-link>
-                    </div>
+                @if(auth()->check())
+                    @if(auth()->user()->admin)
+                        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                            <x-nav-link :href="route('products.create')" :active="request()->routeIs('dashboard')">
+                                {{ __('New Product') }}
+                            </x-nav-link>
+                        </div>
+                    @endif
                 @endif
             </div>
 
@@ -45,7 +48,9 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->first_name }}</div>
+                            @if(auth()->check())
+                                <div>{{ Auth::user()->first_name }}</div>
+                            @endif
 
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -96,10 +101,12 @@
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
+            @if(auth()->check())
+                <div class="px-4">
+                    <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
+                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                </div>
+            @endif
 
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
