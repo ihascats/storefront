b8c35756df658240a0e42caf7f1539ad0cec7f1d
ihascats <s.pavlovic.rs@gmail.com>
2023-09-03 23:08:04 +0200

    Add more details 2 search component

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,16 +22,39 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // Retrieve the unique list of categories from the products
-        $items = Product::all('categories');
+        $items = Product::all('categories', 'price_history', 'variants', 'sizes');
         $combinedCategories = [];
+        $minPrice = PHP_INT_MAX; // Initialize min price as a high value
+        $maxPrice = 0; // Initialize max price as a low value
+        $sizes = [];
+        $colors = [];
+
         foreach ($items as $item) {
-        $categories = $item->categories;
-        $combinedCategories = array_merge($combinedCategories, $categories);
+            $categories = $item->categories;
+            $combinedCategories = array_merge($combinedCategories, $categories);
+
+            // Update min and max prices
+            $minPrice = min($minPrice, $item->price_history[0]['price']);
+            $maxPrice = max($maxPrice, $item->price_history[0]['price']);
+
+            // Collect sizes and colors
+            foreach ($item->variants as $variant) {
+                $sizes = array_merge($sizes, $variant['sizes']);
+                $colors[] = $variant['color'];
+            }
         }
 
         $uniqueCombinedCategories = array_unique($combinedCategories);
+        $uniqueSizes = array_unique($sizes);
+        $uniqueColors = array_unique($colors);
         
-        // Share the categories with all views
-        View::share('categories', $uniqueCombinedCategories);
+        // Share the data with all views
+        View::share([
+            'categories' => $uniqueCombinedCategories,
+            'minPrice' => $minPrice,
+            'maxPrice' => $maxPrice,
+            'sizes' => $uniqueSizes,
+            'colors' => $uniqueColors,
+        ]);
     }
 }
--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,17 +1,86 @@
 <form action="/search" method="GET" class="flex flex-col gap-4">
-    <label for="categories">
+    <label for="query">
         <input type="text" name="query" placeholder="Search...">
         <button type="submit">Search</button>
     </label>
-    <label for="categories">Categories
-        <ul id="categoriesList" class="flex flex-col">
-            @if($categories)
-            @foreach ( $categories as $category)
-            <label for='categories[]'>
-                {{ $category }}
-                <input type='checkbox' name='categories[]' value='{{$category}}'>
-            </label>
-            @endforeach
+    <label for="min-price" class="flex flex-col">Min Price:
+        <input type="number" name="min-price" id="min-price" step="0.01" min="{{$minPrice}}" max="{{$maxPrice}}" value="{{$minPrice}}">
+        <input type="range" name="min-priceSlider" id="min-priceSlider" step="0.01" min="{{$minPrice}}" max="{{$maxPrice}}" value="{{$minPrice}}">
+    </label>
+    <label for="max-price" class="flex flex-col">Max Price:
+        <input type="number" name="max-price" id="max-price" step="0.01" min="{{$minPrice}}" max="{{$maxPrice}}" value="{{$maxPrice}}">
+        <input type="range" name="max-priceSlider" id="max-priceSlider" step="0.01" min="{{$minPrice}}" max="{{$maxPrice}}" value="{{$maxPrice}}">
+    </label>
+    <script>
+        // Get references to the input and slider elements
+        const maxPriceInput = document.getElementById('max-price');
+        const maxPriceSlider = document.getElementById('max-priceSlider');
+        const minPriceInput = document.getElementById('min-price');
+        const minPriceSlider = document.getElementById('min-priceSlider');
+        
+        // Add an event listener to the slider
+        maxPriceSlider.addEventListener('input', function () {
+            // Update the input field's value when the slider changes
+            maxPriceInput.value = maxPriceSlider.value;
+        });
+
+        // Add an event listener to the input field
+        maxPriceInput.addEventListener('input', function () {
+            // Update the slider's value when the input field changes
+            maxPriceSlider.value = maxPriceInput.value;
+        });
+        // Add an event listener to the slider
+        minPriceSlider.addEventListener('input', function () {
+            // Update the input field's value when the slider changes
+            minPriceInput.value = minPriceSlider.value;
+        });
+
+        // Add an event listener to the input field
+        minPriceInput.addEventListener('input', function () {
+            // Update the slider's value when the input field changes
+            minPriceSlider.value = minPriceInput.value;
+        });
+    </script>
+    
+    <label for="categories">Categories</label>
+    <ul id="categoriesList" class="flex flex-col">
+        @if($categories)
+        @foreach($categories as $category)
+        <label for="category_{{$category}}">
+            {{ $category }}
+            <input type="checkbox" id="category_{{$category}}" name="categories[]" value="{{$category}}">
+        </label>
+        @endforeach
+        @endif
+    </ul>
+
+
+    <label for="sizes">Sizes
+        <ul>
+            @if($sizes)
+                @foreach($sizes as $size)
+                    <li>
+                        <label for="sizes[]">
+                            {{ $size }}
+                            <input type="checkbox" name="sizes[]" value="{{ $size }}">
+                        </label>
+                    </li>
+                @endforeach
+            @endif
+        </ul>
+    </label>
+
+    <label for="colors">Colors
+        <ul>
+            @if($colors)
+                @foreach($colors as $color)
+                    <li>
+                        <label for="colors[]">
+                            {{ $color }}
+                            <input type="checkbox" name="colors[]" value="{{ $color }}">
+                        </label>
+                    </li>
+                @endforeach
             @endif
         </ul>
     </label>
